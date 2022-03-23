220b4bd1d42cd51d86a60454d0952ce9ab8c8ab0
Anna Krasnovid <krasnovidanka@yandex.ru>
2022-03-23 15:49:10 +0300

    err 401

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const { createUser, login } = require('./controllers/users');
 const errorHandler = require('./middlewares/middlewares');
 const ErrorNotFound = require('./error/ErrorNotFound');
 const validation = require('./middlewares/validation');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 
@@ -16,8 +17,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
 
-app.use('/users', require('./routes/users'));
-app.use('/cards', require('./routes/cards'));
+app.use('/users', auth, require('./routes/users'));
+app.use('/cards', auth, require('./routes/cards'));
 
 app.post('/signin', validation.checkLogin, login);
 app.post('/signup', validation.checkUserCreate, createUser);
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
-const auth = require('../middlewares/auth');
+// const auth = require('../middlewares/auth');
 const validation = require('../middlewares/validation');
 const { getUsers, updateUserInfo, getCurrentUsers, getUsersId, updateUserAvatar } = require('../controllers/users');
 
-router.get('/', auth, getUsers);
-router.patch('/me', auth, validation.checkUserInfo, updateUserInfo);
-router.get('/me', auth, getCurrentUsers);
-router.patch('/me/avatar', auth, validation.checkUserAvatar, updateUserAvatar);
-router.get('/:userId', auth, validation.checkUserId, getUsersId);
+router.get('/', getUsers);
+router.patch('/me', validation.checkUserInfo, updateUserInfo);
+router.get('/me', getCurrentUsers);
+router.patch('/me/avatar', validation.checkUserAvatar, updateUserAvatar);
+router.get('/:userId', validation.checkUserId, getUsersId);
 
 module.exports = router;
