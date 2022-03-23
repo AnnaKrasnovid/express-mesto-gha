5df697be76602b50347d724b3dc6a90804566526
Anna Krasnovid <krasnovidanka@yandex.ru>
2022-03-23 15:37:25 +0300

    401

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', { useNewUrlParser: true });
 
-app.post('/signin', validation.checkLogin, login);
-app.post('/signup', validation.checkUserCreate, createUser);
-
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
+
+app.post('/signin', validation.checkLogin, login);
+app.post('/signup', validation.checkUserCreate, createUser);
 
 app.use((req, res, next) => {
   next(new ErrorNotFound('Not found'));
